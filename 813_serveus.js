7b8a65637cc23489850bf26307db498a8277e2db
Jenny Astrachan <jennya@mit.edu>
2014-05-05 16:45:57 -0400

    changed js to delete hello world stuff and removed packages directory

--- 813_serveus.js
+++ 813_serveus.js
@@ -11,17 +11,6 @@ Router.map(function() {
 });
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to 813_serveus.";
-  };
-
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
 
   // Menu functions
   $('#suggest_submit').click(function() {
