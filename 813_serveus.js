e576456e62e8fe946d87a49b8f0b6f4f34fefe13
Jenny Astrachan <jennya@mit.edu>
2014-05-05 16:39:38 -0400

    committing menu updates

--- 813_serveus.js
+++ 813_serveus.js
@@ -1,3 +1,6 @@
+Menu = new Meteor.Collection("menu");
+
+
 Router.configure();
 
 Router.map(function() {
@@ -19,10 +22,33 @@ if (Meteor.isClient) {
         console.log("You pressed the button");
     }
   });
+
+  // Menu functions
+  $('#suggest_submit').click(function() {
+    var suggestion = $('#suggest_box').val();
+    if (suggestion != ''){
+      $('#suggest_box').val('');
+      var li_string = '<li class = "list-group-item">'+suggestion+'</li>';
+      console.log(li_string);
+      $('.suggestions').prepend(li_string);
+      $('.suggestions li:last-child').remove();
+    }
+  });
+
+  Template.menu.menu_days = function() {
+    return Menu.find({});
+  };
+  Template.menu.menu_items = function() {
+
+  }
+  
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+    if (Menu.find().count() === 0) {
+      Menu.insert({day: "Sunday", date: "5/4", items:"item1", items: "item2"});
+    }
   });
 }
