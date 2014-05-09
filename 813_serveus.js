23bc73e98f5a6c671522dfe36c0fd15b14a9f9b9
Eann Tuan <eanntuan@Eanns-iMac-2.local>
2014-05-09 04:46:19 -0400

    like and dislike functional

--- 813_serveus.js
+++ 813_serveus.js
@@ -64,9 +64,15 @@ if (Meteor.isClient) {
  
  Template.requests.events({
    'click #deleteButton' : function () {
-      console.log("trying to delete");
+    //  console.log("trying to delete");
       Requests.remove(this._id);
-    }
+    },
+   'click #liked' : function(){
+      Requests.update (this._id, {$inc: {likes:1}});
+   },
+   'click #disliked' : function(){
+      Requests.update (this._id, {$inc: {unlikes:1}});
+   },
   });
  
   // Menu functions
@@ -152,168 +158,17 @@ if (Meteor.isClient) {
   
   // requests functions
   Template.requests.rendered = function(){
-		  // add in all items that have been requested
-		  
-	//console.log("length ");
-	//console.log ("is" + Requests.count());
-	
-
-
 
   $(newRequest).val('');
-//		$('#houseTeam').prop('disabled', false);
-
-
-
 
 $('#newRequest').focus();
-		/*var allRequests = new Array();
-		var requestNames = new Array();
-		allRequests.push("strawberries");
-		allRequests.push ("pitachips");
-	
-	
-		// start out with 2 stickies already
-		// strawberries
-		var request = 'strawberries';
-		var requestName = 'strawberries';
-		$('#stickies').append("<li class = 'col-xs-3' id = 'requestSticky'>" +
-				"<div style = 'margin-bottom: 45%;' class='thumbnail' id='sticky" + request+"'>" +
-				"<img src= 'stickynote.png' id = 'stickyImage'>" +
-				"<div class = 'requestedItem'>" + // style = 'margin-top: -95%'>" +
-				"<span style = 'margin-left:75%; color: #FF5959;' id = 'delete"+request+"'  > </span> <br>" +
-					"<h3 style ='cursor: default' id = '"+request+"text'>" + requestName + " </h3>" + 
-					"<div class = 'btn-toolbar' style= 'margin-left: 20%; margin-right: 20%'>" + 
-							"<div class = 'btn-group' id ='buttons'>"+
-								"<span class='badge-info' id = 'liked"+request+"'> <span class='glyphicon glyphicon-thumbs-up' style= 'cursor:default;'>" +
-								" </span> </span>" + 
-								"<span 'class='badge-info' id = 'numLiked" +request+"'>1</span> </div>" +
-								"<span width='100px'> </span>" + 
-							"<div class = 'btn-group'>"+
-								"<span class='badge-info' id = 'disliked"+request+"'> <span class='glyphicon glyphicon-thumbs-down'>" +
-								" </span> </span>" + 
-								"<span class='badge-info' id ='numDisliked" + request+"'>0</span> </div>" + 
-						"</div> </div> </div> </li> ");
-			$("#likedstrawberries").click(function(evt){
-				var currNum = parseInt($("#numLikedstrawberries").text());
-				var newNum = currNum + 1;
-				$("#numLikedstrawberries").text(newNum);
-			});
-			$("#dislikedstrawberries").click(function(evt){
-				var currNum = parseInt($("#numDislikedstrawberries").text());
-				var newNum = currNum + 1;
-				$("#numDislikedstrawberries").text(newNum);
-			});
-  
-		// pita chips
-		var request = 'pitachips';
-		var requestName = 'pita chips';
-		$('#stickies').append("<li class = 'col-xs-3' id = 'requestSticky'>" +
-		"<div style = 'margin-bottom: 45%;' class='thumbnail' id='sticky" + request+"'>" +
-				"<img src= 'stickynote.png' id = 'stickyImage'>" +
-				"<div class = 'requestedItem'>" + // style = 'margin-top: -95%'>" +
-				"<span style = 'margin-left:75%; color: #FF5959;' id = 'delete"+request+"'  > </span> <br>" +
-					"<h3 style ='cursor: default' id = '"+request+"text'>" + requestName + " </h3>" + 
-					"<div class = 'btn-toolbar' style= 'margin-left: 7%; margin-right: 20%; margin-top:30%;'>" + 
-							"<div class = 'btn-group' id ='buttons'>"+
-								"<span class='badge-info' id = 'liked"+request+"'> <span class='glyphicon glyphicon-thumbs-up'>" +
-								" </span> </span>" + 
-								"<span class='badge-info' id = 'numLiked" +request+"'>1</span> </div>" +
-							 
-							"<div style= 'position: absolute; left: 43%' class = 'btn-group'>"+
-								"<span class='badge-info' id = 'disliked"+request+"'> <span class='glyphicon glyphicon-thumbs-down'>" +
-								" </span> </span>" + 
-								"<span class='badge-info' id ='numDisliked" + request+"'>0</span> </div>" + 
-						"</div> </div> </div> </li> ")
-			$("#likedpitachips").click(function(evt){
-				var currNum = parseInt($("#numLikedpitachips").text());
-				var newNum = currNum + 1;
-				$("#numLikedpitachips").text(newNum);
-			});
-			$("#dislikedpitachips").click(function(evt){
-				var currNum = parseInt($("#numDislikedpitachips").text());
-				var newNum = currNum + 1;
-				$("#numDislikedpitachips").text(newNum);
-			});
-			//console.log(" here 1");
-		if ( Meteor.user() != null){
-			if (Meteor.user().profile.name == "House Member Julia") {
-				//console.log("here 2");
-				for (var i =0; i< allRequests.length; i++) {
-					var request = allRequests[i];
-					$('#'+request+'text').attr('contenteditable',"true");
-					$('#'+request+'text').css('cursor', 'text' );
-					$('#delete'+request).append("<button class = 'btn btn-sm' style='background-color:transparent' id = 'deleted" + request+"'> <span style = 'margin-left:-7px' class = 'glyphicon glyphicon-remove'></span> </button>");
-					$('#deleted'+request).click(function(evt){
-						$(this).parents('li').remove();	
-					});
-				}
-			}
-		};
-		//console.log("here 3");*/
-
-
-
-
-
-		$('#newRequest').focus();
 		$("#btnSubmit").click(function(evt) {
-			//console.log(Requests.find().count(0).name);
-			//console.log("pressed submit button"); 
 			var requestName = $(newRequest).val();
 			var request = $(newRequest).val().replace(/[ .,!?]/g,'');
 			if (request == '') {return;}
 			
-			Requests.insert({name: ""+requestName+"", item:""+request+"", likes: "1", unlikes: "0"});
-			
-			//var liked;
-			//var disliked;
-			$('#stickies').append("<li class = 'col-xs-3' id = 'requestSticky'>" +
-				"<div style = 'margin-bottom: 45%;' class='thumbnail' id='sticky" + request+"'>" +
-				"<img src= 'stickynote.png' id = 'stickyImage'>" +
-				"<div class = 'requestedItem'>"+ // style = 'margin-top: -95%'>" +
-				"<span style = 'margin-left:75%; color: #FF5959;' id = 'delete"+request+"'  > </span> <br>" +
-       "<h3 style ='cursor: default' id = '"+request+"text'>" + requestName + " </h3>" + 
-       "<div class = 'btn-toolbar' style= 'margin-left: 10%; margin-right: 10%'>" + 
-       "<div class = 'btn-group' id ='buttons'>"+
-       "<span class='badge-info' id = 'liked"+request+"'> <span class='glyphicon glyphicon-thumbs-up'>" +
-       " </span> </span>" + 
-       "<span class='badge-info' id = 'numLiked" +request+"'>1</span> </div>" +
-       "<div style= 'position: absolute; left: 57%' class = 'btn-group'>"+
-       "<span class='badge-info' id = 'disliked"+request+"'> <span class='glyphicon glyphicon-thumbs-down'>" +
-       " </span> </span>" + 
-       "<span class='badge-info' id ='numDisliked" + request+"'>0</span> </div>" + 
-       "</div> </div> </div> </li> ");
-
-$("#liked"+request).click(function(evt){
-  var req = Requests.findOne({name: request});
-  var num = 1+parseInt(req.likes);
-  $("#numLiked" + request).text(num);
-				//liked = newNum;
-			});
-$("#disliked"+request).click(function(evt){
-  var req = Requests.findOne({name: request});
-  var num = parseInt(req.likes) -1;
-  $("#numDisliked" + request).text(num);
-				//disliked = newNum;
-			});
-
-			/*
-			// fix stuff if you're a special user
-			if ( Meteor.user() != null){
-				if (Meteor.user().profile.name == "House Member Julia") {
-			// fix so works with new thing
-				$('#delete'+request).append("<button  class = 'btn btn-sm' style='background-color:transparent; margin-left:-7px' id = 'deleted" + request+"'> <span class = 'glyphicon glyphicon-remove'></span> </button>");
-				$('#deleted'+request).click(function(evt){
-					$(this).parents('li').remove();	
-				});
-				$('#'+request+'text').attr('contenteditable',"true");
-				$('#'+request+'text').css('cursor', 'text' );
-			}
-   }*/
+			Requests.insert({name: ""+requestName+"", item:""+request+"", likes: 1, unlikes: 0, time_created: Date.now()});
    $(newRequest).val(''); 
-
-   console.log( "newest added " + req.name);
 
  }); 
 };
