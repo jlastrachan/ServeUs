22c916b97b9a2b89032f84ddfecb89f78373ee04
Eann Tuan <eanntuan@Eanns-iMac-2.local>
2014-05-09 02:09:48 -0400

    eann

--- 813_serveus.js
+++ 813_serveus.js
@@ -14,9 +14,37 @@ Router.map(function() {
 
 if (Meteor.isClient) {
 
-Template.requests.requests = function () {
-	return Requests.find({});
-};
+  //Home functions
+  Template.home.rendered = function(){
+    $('#contactSticky').mouseenter(function(){
+      $(this).prepend("<div class = 'text-center' id = 'contactInfo'> Contact members of the house team </div>");
+    });
+
+    $('#contactSticky').mouseleave (function(){
+      $('#contactInfo').remove();
+    });
+
+    $('#menuSticky').mouseenter(function(){
+      $(this).prepend("<div class = 'text-center' id = 'menuInfo'> View this week's menu and comment on recent dinners </div>");
+    });
+
+    $('#menuSticky').mouseleave (function(){
+      $('#menuInfo').remove();
+    });
+
+    $('#requestSticky').mouseenter(function(){
+      $(this).prepend("<div class = 'text-center' id = 'requestInfo'> View current food requests and add your own </div>");
+    });
+
+    $('#requestSticky').mouseleave (function(){
+      $('#requestInfo').remove();
+    });
+
+  };
+
+  Template.requests.requests = function () {
+   return Requests.find({});
+ };
   // Menu functions
   Template.menu.rendered = function() {
     $('#suggest_submit').click(function() {
@@ -101,21 +129,21 @@ Template.requests.requests = function () {
   
   
   // requests functions
-	Template.requests.rendered = function(){
+  Template.requests.rendered = function(){
 		  // add in all items that have been requested
 		  
 	//console.log("length ");
 	//console.log ("is" + Requests.count());
 	
-  
-    
-  	
-		$(newRequest).val('');
+
+
+
+  $(newRequest).val('');
 //		$('#houseTeam').prop('disabled', false);
-	
-	
-	
-		$('#newRequest').focus();
+
+
+
+$('#newRequest').focus();
 		/*var allRequests = new Array();
 		var requestNames = new Array();
 		allRequests.push("strawberries");
@@ -217,31 +245,31 @@ Template.requests.requests = function () {
 				"<img src= 'stickynote.png' id = 'stickyImage'>" +
 				"<div class = 'requestedItem'>"+ // style = 'margin-top: -95%'>" +
 				"<span style = 'margin-left:75%; color: #FF5959;' id = 'delete"+request+"'  > </span> <br>" +
-					"<h3 style ='cursor: default' id = '"+request+"text'>" + requestName + " </h3>" + 
-					"<div class = 'btn-toolbar' style= 'margin-left: 10%; margin-right: 10%'>" + 
-							"<div class = 'btn-group' id ='buttons'>"+
-								"<span class='badge-info' id = 'liked"+request+"'> <span class='glyphicon glyphicon-thumbs-up'>" +
-								" </span> </span>" + 
-								"<span class='badge-info' id = 'numLiked" +request+"'>1</span> </div>" +
-							"<div style= 'position: absolute; left: 57%' class = 'btn-group'>"+
-								"<span class='badge-info' id = 'disliked"+request+"'> <span class='glyphicon glyphicon-thumbs-down'>" +
-								" </span> </span>" + 
-								"<span class='badge-info' id ='numDisliked" + request+"'>0</span> </div>" + 
-						"</div> </div> </div> </li> ");
-			
-			$("#liked"+request).click(function(evt){
-				var req = Requests.findOne({name: request});
-				var num = 1+parseInt(req.likes);
-				$("#numLiked" + request).text(num);
+       "<h3 style ='cursor: default' id = '"+request+"text'>" + requestName + " </h3>" + 
+       "<div class = 'btn-toolbar' style= 'margin-left: 10%; margin-right: 10%'>" + 
+       "<div class = 'btn-group' id ='buttons'>"+
+       "<span class='badge-info' id = 'liked"+request+"'> <span class='glyphicon glyphicon-thumbs-up'>" +
+       " </span> </span>" + 
+       "<span class='badge-info' id = 'numLiked" +request+"'>1</span> </div>" +
+       "<div style= 'position: absolute; left: 57%' class = 'btn-group'>"+
+       "<span class='badge-info' id = 'disliked"+request+"'> <span class='glyphicon glyphicon-thumbs-down'>" +
+       " </span> </span>" + 
+       "<span class='badge-info' id ='numDisliked" + request+"'>0</span> </div>" + 
+       "</div> </div> </div> </li> ");
+
+$("#liked"+request).click(function(evt){
+  var req = Requests.findOne({name: request});
+  var num = 1+parseInt(req.likes);
+  $("#numLiked" + request).text(num);
 				//liked = newNum;
 			});
-			$("#disliked"+request).click(function(evt){
-				var req = Requests.findOne({name: request});
-				var num = parseInt(req.likes) -1;
-				$("#numDisliked" + request).text(num);
+$("#disliked"+request).click(function(evt){
+  var req = Requests.findOne({name: request});
+  var num = parseInt(req.likes) -1;
+  $("#numDisliked" + request).text(num);
 				//disliked = newNum;
 			});
-			
+
 			/*
 			// fix stuff if you're a special user
 			if ( Meteor.user() != null){
@@ -254,16 +282,16 @@ Template.requests.requests = function () {
 				$('#'+request+'text').attr('contenteditable',"true");
 				$('#'+request+'text').css('cursor', 'text' );
 			}
-			}*/
-			$(newRequest).val(''); 
-			
-			console.log( "newest added " + req.name);
-			
-	}); 
-  };
-	
+   }*/
+   $(newRequest).val(''); 
 
-		};
+   console.log( "newest added " + req.name);
+
+ }); 
+};
+
+
+};
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
